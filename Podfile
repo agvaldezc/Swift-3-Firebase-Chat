e168919123ec401a9a110419f10bbcdb60a92513
Alan Valdez <alan.gvaldez@gmail.com>
2016-09-20 01:12:48 -0500

    Initial commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swiftTestChat' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for swiftTestChat
+  pod 'Firebase/Core'
+  pod 'Firebase/AdMob’
+  pod 'Firebase/Messaging'
+  pod 'Firebase/Database'
+  pod 'Firebase/Invites’
+  pod 'Firebase/DynamicLinks'
+  pod 'Firebase/Crash'
+  pod 'Firebase/RemoteConfig'
+  pod 'Firebase/Auth'
+  pod 'Firebase/AppIndexing'
+  pod 'Firebase/Storage'
+  pod 'JSQMessagesViewController'
+
+end
--- swiftTestChat/ViewController.swift
+++ swiftTestChat/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    var loggedUser : FIRUser!
+    var authReference : FIRAuth!
+    var userID = "default"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        authReference = FIRAuth.auth()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func loginAnonimously(sender: UIButton) {
+        authReference?.signInAnonymouslyWithCompletion() { (user, error) in
+            if error != nil {
+                print(error!.description)
+                return
+            }
+            
+            self.loggedUser = user
+            self.performSegueWithIdentifier("loggedIn", sender: self)
+        }
+    }
 }
 
