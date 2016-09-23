a1b0bf1287f3331670df9e2c7e59cdf85a285d9c
Alan Valdez <agvaldezc@Alans-MBP.Home>
2016-09-22 19:23:11 -0500

    Uploaded project working as public chat with anonymous login

--- swiftTestChat/ChatViewController.swift
+++ swiftTestChat/ChatViewController.swift
@@ -2,93 +2,145 @@
 //  ChatViewController.swift
 //  swiftTestChat
 //
-//  Created by Alan Valdez on 9/19/16.
+//  Created by Alan Valdez on 9/20/16.
 //  Copyright © 2016 ITESM. All rights reserved.
 //
 
 import UIKit
+import Firebase
+import JSQMessagesViewController
 
-private let reuseIdentifier = "Cell"
-
-class ChatViewController: UICollectionViewController {
-
+class ChatViewController: JSQMessagesViewController {
+    
+    var messages = [JSQMessage]()
+    var incomingBubbleImageView: JSQMessagesBubbleImage!
+    var outgoingBubbleImageView: JSQMessagesBubbleImage!
+    let databaseRef = FIRDatabase.database().reference(fromURL: "https://swift-chat-test-ab7a4.firebaseio.com")
+    
+    var messageRef : FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
+        
         // Do any additional setup after loading the view.
+        
+        setupBubbles()
+        
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+        
+        //Name the object inside the database
+        messageRef = databaseRef.child("sentMessages")
+        
+        // messages from someone else
+        addMessage(id: "foo", text: "Hey person!")
+        // messages sent from local sender
+        addMessage(id: senderId, text: "Yo!")
+        addMessage(id: senderId, text: "I like turtles!")
+        
+        observeMessages()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
-    }
-
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
-        // Configure the cell
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            return outgoingBubbleImageView
+        } else {
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            cell.textView!.textColor = UIColor.white
+        } else {
+            cell.textView!.textColor = UIColor.black
+        }
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
     
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        
+        let itemRef = messageRef.childByAutoId()
+        let messageItem = [
+            "text": text!,
+            "senderId": senderId!
+        ]
+        
+        itemRef.setValue(messageItem)
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+        
+        finishSendingMessage()
     }
-    */
-
+    
+    private func setupBubbles() {
+        let factory = JSQMessagesBubbleImageFactory()
+        
+        outgoingBubbleImageView = factory?.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+        
+        incomingBubbleImageView = factory?.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }
+    
+    func addMessage(id: String, text: String) {
+        let message = JSQMessage(senderId: id, displayName: "", text: text)
+        
+        messages.append(message!)
+    }
+    
+    private func observeMessages() {
+        let messagesQuery = messageRef.queryLimited(toLast: 25)
+        
+        messagesQuery.observe(.childAdded) { (snapshot: FIRDataSnapshot!) in
+            
+            if let data = snapshot.value as? [String : String] {
+                
+                print(data)
+                
+                let id = data["senderId"]! as String
+                let text = data["text"]! as String
+                
+                self.addMessage(id: id, text: text)
+            }
+            
+            self.finishReceivingMessage()
+        }
+    }
 }
--- swiftTestChat/ViewController.swift
+++ swiftTestChat/ViewController.swift
@@ -28,16 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func loginAnonimously(sender: UIButton) {
-        authReference?.signInAnonymouslyWithCompletion() { (user, error) in
+    @IBAction func loginAnonimously(_ sender: UIButton) {
+        authReference?.signInAnonymously() { (user, error) in
             if error != nil {
-                print(error!.description)
+                print(error)
                 return
             }
             
             self.loggedUser = user
-            self.performSegueWithIdentifier("loggedIn", sender: self)
+            self.performSegue(withIdentifier: "loggedIn", sender: self)
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        let newView = segue.destination as! UINavigationController
+        
+        let chatView = newView.viewControllers.first as! ChatViewController
+        
+        chatView.navigationItem.title = "Swift Firebase Chat"
+        
+        chatView.senderId = self.loggedUser.uid
+        chatView.senderDisplayName = ""
     }
 }
 
